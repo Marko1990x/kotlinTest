6c4e94c0d7c5e5de901804defca63398d9942d27
Marko Dunovic <marko_dunovic@hotmail.com>
2022-03-15 02:43:15 +0100

    add to room works

--- app/src/main/java/markodunovic/web/app/kotlintest/fragments/FragmentAdd.kt
+++ app/src/main/java/markodunovic/web/app/kotlintest/fragments/FragmentAdd.kt
@@ -10,8 +10,11 @@ import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import markodunovic.web.app.kotlintest.MainActivity
 import markodunovic.web.app.kotlintest.R
+import markodunovic.web.app.kotlintest.room.model.Animal
+import markodunovic.web.app.kotlintest.room.viewmodel.PetsViewModel
 import kotlin.math.log
 
 class FragmentAdd : Fragment() {
@@ -20,6 +23,8 @@ class FragmentAdd : Fragment() {
     private lateinit var petName: TextView
     private lateinit var petRace: TextView
     private lateinit var button: Button
+
+    private lateinit var petUserViewModel:PetsViewModel
 
     companion object {
         fun newInstance() = FragmentAdd()
@@ -32,6 +37,7 @@ class FragmentAdd : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val view: View = inflater.inflate(R.layout.fragment_add, container, false)
+        petUserViewModel = ViewModelProvider(this).get(PetsViewModel::class.java)
         numberPicker = view.findViewById(R.id.petAge)
         petName = view.findViewById(R.id.petName)
         petRace = view.findViewById(R.id.petRace)
@@ -45,8 +51,14 @@ class FragmentAdd : Fragment() {
                 Toast.makeText(context, "Data cant be empty", Toast.LENGTH_LONG).show()
             } else {
                 Log.d(TAG, "name ${petName.text} race ${petRace.text} age ${numberPicker.value}")
+                val animal = Animal(0,name,race,numberPicker.value)
+                petUserViewModel.addPet(animal)
+                Toast.makeText(context,"Added to database",Toast.LENGTH_SHORT).show()
+
             }
         })
         return view
     }
+
+
 }
